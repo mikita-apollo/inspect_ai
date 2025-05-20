65dc5fa46e174f4beedb264172edf9aeb07e4e19
dragonstyle <cteague@gmail.com>
2025-05-20 13:08:25 -0400

    tree cleanup

--- src/inspect_ai/_view/www/dist/assets/index.js
+++ src/inspect_ai/_view/www/dist/assets/index.js
@@ -54785,8 +54785,8 @@ self.onmessage = function (e) {
       let modelNode = null;
       const toolNodes = [];
       let turnCount = 1;
-      const makeTurn = () => {
-        if (modelNode !== null) {
+      const makeTurn = (force) => {
+        if (modelNode !== null && (force || toolNodes.length > 0)) {
           const turnNode = new EventNode(
             modelNode.id,
             {
@@ -54804,14 +54804,14 @@ self.onmessage = function (e) {
           );
           turnNode.children = [modelNode, ...toolNodes];
           results.push(turnNode);
-          modelNode = null;
-          toolNodes.length = 0;
         }
+        modelNode = null;
+        toolNodes.length = 0;
       };
       for (const node2 of eventNodes) {
         if (node2.event.event === "model") {
           if (modelNode !== null && toolNodes.length === 0) {
-            makeTurn();
+            makeTurn(true);
           } else {
             makeTurn();
             modelNode = node2;
@@ -54823,6 +54823,7 @@ self.onmessage = function (e) {
           results.push(node2);
         }
       }
+      makeTurn();
       return results;
     };
     const collapseTurns = (eventNodes) => {
@@ -54899,8 +54900,6 @@ self.onmessage = function (e) {
             removeNodeVisitor("input"),
             // Strip the sandbox wrapper (and children)
             removeStepSpanNameVisitor(kSandboxSignalName),
-            // Remove any leftover bare model calls that aren't in turns
-            // removeNodeVisitor("model"),
             // Remove child events for scorers
             noScorerChildren()
           ]
